2f1d08836008b27426b71c9a409e36aa174bd32a
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-02-27 12:13:24 +0100

    merge_1

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -12,9 +12,10 @@ export const Map = () => {
       try {
         const token = localStorage.getItem("access_token"); // Récupération du token JWT
         const headers = token ? { Authorization: `Bearer ${token}` } : {};
-        
+
         const response = await fetch(API_URL, { headers });
-        if (!response.ok) throw new Error("Erreur lors du chargement des données");
+        if (!response.ok)
+          throw new Error("Erreur lors du chargement des données");
 
         const data = await response.json();
         setDenonciations(data);
@@ -27,32 +28,40 @@ export const Map = () => {
   }, []);
 
   return (
-    <MapContainer 
-      center={[48.8566, 2.3522]} 
-      zoom={13} 
-      minZoom={2}  // Permet de dézoomer mais pas en-dessous du niveau 10
-      maxZoom={13}  // Empêche de zoomer au-delà du niveau 13
-      scrollWheelZoom={false}  // Activation du zoom à la molette
-      touchZoom={true}  // Activation du zoom tactile
-      doubleClickZoom={true}  // Activation du zoom au double-clic
-      style={{ height: "500px", width: "100%" }}
+    <MapContainer
+      center={[48.8566, 2.3522]}
+      zoom={13}
+      minZoom={2} // Permet de dézoomer mais pas en-dessous du niveau 10
+      maxZoom={13} // Empêche de zoomer au-delà du niveau 13
+      scrollWheelZoom={false} // Activation du zoom à la molette
+      touchZoom={true} // Activation du zoom tactile
+      doubleClickZoom={true} // Activation du zoom au double-clic
+      style={{
+        height: "500px",
+        width: "100%",
+        marginBottom: "30px",
+      }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {denonciations.map((denonciation) => (
-        denonciation.point && (
-          <Marker 
-            key={denonciation.id} 
-            position={[denonciation.point.coordinates[1], denonciation.point.coordinates[0]]}
-          >
-            <Popup>
-              <strong>Catégorie :</strong> {denonciation.categorie}
-              <br />
-              <strong>Localisation :</strong> {denonciation.localisation}
-            </Popup>
-          </Marker>
-        )
-      ))}
+      {denonciations.map(
+        (denonciation) =>
+          denonciation.point && (
+            <Marker
+              key={denonciation.id}
+              position={[
+                denonciation.point.coordinates[1],
+                denonciation.point.coordinates[0],
+              ]}
+            >
+              <Popup>
+                <strong>Catégorie :</strong> {denonciation.categorie}
+                <br />
+                <strong>Localisation :</strong> {denonciation.localisation}
+              </Popup>
+            </Marker>
+          )
+      )}
     </MapContainer>
   );
 };
