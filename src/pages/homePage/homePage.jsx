d6de0db59354b05bbe32197624dd75c71269955d
Tom Julliat <tom.julliat@next-u.fr>
2025-02-27 11:48:57 +0100

    ✨ Ajout de la carte interactive avec récupération des dénonciations

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -2,7 +2,9 @@ import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
 import { HeroSection } from "../../components/heroSection/heroSection";
 import { InfoCards } from "../../components/homeSections/homeSections";
+import  { Map }  from "../../components/map/map";
 import "./homePage.css";
+import { MapContainer } from "react-leaflet";
 // import { DossiersEnquetes } from "./components/dossiersEnquetes/dossiersEnquetes";
 // import { Actualites } from "./components/actualites/actualites";
 
@@ -11,6 +13,7 @@ export const HomePage = () => {
     <div>
       <HeroSection />
       <InfoCards />
+      <Map />
     </div>
   );
 };
