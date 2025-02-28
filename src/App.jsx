0305950b1fd7eec9353bd74553f5e7c2a25f5569
matteolvt <146728526+matteolvt@users.noreply.github.com>
2025-02-28 11:34:38 +0100

    resolve prob

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
 import { HomePage } from "./pages/homePage/homePage";
 import { VosDemarches } from "./pages/vosDemarches/vosDemarches";
 import { Header } from "./components/header/header";
@@ -8,8 +14,10 @@ import { SignalerUnFait } from "./pages/signalerUnFait/signalerUnFait";
 import { AuthPage } from "./pages/login/login";
 import Dashboard from "./pages/dashboard/dashboard";
 import Forbidden from "./pages/Forbidden.jsx/Forbidden";
-import { Actualites } from "./pages/actualites/actualites";
-import { Notifications } from "./components/notifications/notifications";
+import { Actualites } from "./pages/actualites/actualites"; // Nouvelle route Actualites
+import { Notifications } from "./components/notifications/notifications"; // Importation du composant Notifications
+import { Propos } from "./pages/propos/propos";
+import { InfosRessources } from "./pages/InfosRessources/InfoRessources";
 
 const AppContent = () => {
   const location = useLocation();
@@ -23,23 +31,24 @@ const AppContent = () => {
 
   return (
     <>
-      {!hideHeaderFooter && <Header toggleNotifications={() => {}} notifications={notifications} />} 
+      {!hideHeaderFooter && (
+        <Header toggleNotifications={() => {}} notifications={notifications} />
+      )}
       <Notifications notifications={notifications} />
       <div className="p-0">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/vos-demarches" element={<VosDemarches addNotification={addNotification} />} />
+          <Route
+            path="/vos-demarches"
+            element={<VosDemarches addNotification={addNotification} />}
+          />
           <Route path="/signaler" element={<SignalerUnFait />} />
           <Route path="/connexion" element={<AuthPage />} />
           <Route path="/actualites" element={<Actualites />} />
           <Route
             path="/dashboard"
             element={
-              isAuthenticated ? (
-                <Dashboard />
-              ) : (
-                <Navigate to="/connexion" />
-              )
+              isAuthenticated ? <Dashboard /> : <Navigate to="/connexion" />
             }
           />
           <Route path="/forbidden" element={<Forbidden />} />
